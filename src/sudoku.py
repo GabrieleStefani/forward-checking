5728131ace8690f7ab19aeb86d6798a7458e94ae
Gabriele Stefani <gabriele.stefani@toconsulting.it>
2020-02-05 19:09:17 +0100

    added sudoku varables and domains

--- src/sudoku.py
+++ src/sudoku.py
@@ -1,4 +1,4 @@
-import csv
+import numpy as np
 import csp
 
 
@@ -7,11 +7,13 @@ def main():
     domains = {}
     neighbors = {}
 
-
-with open('sudoku.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        for elem in row:
+    sudoku = list(
+        "004300209005009001070060043006002087190007400050083000600000105003508690042910300")
+    for var in len(sudoku):
+        variables.append(var)
+        if var != 0:
+            domains[var] = [sudoku[var]]
+        neighbors[var] = []
 
     def constraint(A, a, B, b):
         return True
